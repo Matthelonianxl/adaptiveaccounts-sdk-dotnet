4d281ca3660f3444a491a5a8e352e2f7a0fb9c13
unknown <rsomaskandan@D-MAA-00391891.corp.ebay.com>
2012-12-21 20:43:13 +0530

    Updated for NuGet

--- AdaptiveAccounts.bat
+++ AdaptiveAccounts.bat
@@ -1,3 +1,3 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 8\Common7\IDE\devenv.com" PayPal_AdaptiveAccounts_SDK.sln /build Release %1
+call "C:\Program Files (x86)\Microsoft Visual Studio 8\Common7\IDE\devenv.com" PayPal_AdaptiveAccounts_SDK\PayPal_AdaptiveAccounts_SDK.sln /build Release %1
 
 copy /Y PayPal_AdaptiveAccounts_SDK\bin\Release\PayPal_AdaptiveAccounts_SDK.dll Samples\AdaptiveAccountsSampleApp\lib\. 
