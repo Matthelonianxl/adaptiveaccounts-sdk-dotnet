56e59f3fbcdf00bcf43f4e4b265ce90cdee09ea8
unknown <rsomaskandan@D-MAA-00391891.corp.ebay.com>
2012-12-27 00:02:07 +0530

    Removed underscore from folders as part of .NET Standards

--- AdaptiveAccounts.bat
+++ AdaptiveAccounts.bat
@@ -1,3 +1,3 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 8\Common7\IDE\devenv.com" PayPal_AdaptiveAccounts_SDK\PayPal_AdaptiveAccounts_SDK.sln /build Release %1
+call "C:\Program Files (x86)\Microsoft Visual Studio 8\Common7\IDE\devenv.com" PayPalAdaptiveAccountsSDK\PayPalAdaptiveAccountsSDK.sln /build Release
 
-copy /Y PayPal_AdaptiveAccounts_SDK\bin\Release\PayPalAdaptiveAccountsSDK.dll Samples\AdaptiveAccountsSampleApp\lib\. 
+copy /Y PayPalAdaptiveAccountsSDK\bin\Release\PayPalAdaptiveAccountsSDK.dll Samples\AdaptiveAccountsSampleApp\lib\. 
