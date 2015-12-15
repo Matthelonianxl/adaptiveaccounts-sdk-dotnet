6ad6a8a0859d4077e451d47f90b4fe298b8343c3
unknown <jziaja@paypalcorp.com>
2015-12-14 22:35:23 -0600

    Update Core SDK dependency

--- AdaptiveAccounts.bat
+++
@@ -1,4 +0,0 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 8\Common7\IDE\devenv.com" PayPalAdaptiveAccountsSDK\PayPalAdaptiveAccountsSDK.sln /build Release
-
-copy /Y PayPalAdaptiveAccountsSDK\bin\Release\PayPalAdaptiveAccountsSDK.dll Samples\AdaptiveAccountsSampleApp\lib\. 
-copy /Y PayPalAdaptiveAccountsSDK\bin\Release\PayPalAdaptiveAccountsSDK.XML Samples\AdaptiveAccountsSampleApp\lib\. 
